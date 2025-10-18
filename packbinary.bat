726b9f9eb66d7127c344a9dbb200d8e9a689f31c
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-10-18 15:25:27 -0500

    revert rename

--- packbinary.bat
+++ packbinary.bat
@@ -1,5 +1,5 @@
-copy Moto_Logo\bin\Release\MotoBootLogoMaker.exe MotoBootLogoMaker.exe
-copy Moto_Logo\bin\Release\MotoBootLogoMaker.exe.config MotoBootLogoMaker.exe.config
+copy Moto_Logo\bin\Release\Moto_Boot_Logo_Maker.exe MotoBootLogoMaker.exe
+copy Moto_Logo\bin\Release\Moto_Boot_Logo_Maker.exe.config MotoBootLogoMaker.exe.config
 7z a -tZip -y MotoBootLogoMaker.zip MotoBootLogoMaker.exe MotoBootLogoMaker.exe.config
 del MotoBootLogoMaker.exe
 del MotoBootLogoMaker.exe.config
